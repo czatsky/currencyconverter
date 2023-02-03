6d1a69e20b1e894cb708047c24e354c142891449
Violetta Filatova <47822059+czatsky@users.noreply.github.com>
2023-02-03 11:55:13 +0300

    Update cconverter.py

--- Currency Converter/task/cconverter/cconverter.py
+++ Currency Converter/task/cconverter/cconverter.py
@@ -1,30 +1,3 @@
-# write your code here!
-# coins = float(input())
-# rub = 2.98
-# rubname = 'RUB'
-# ars = 0.82
-# arsname = 'ARS'
-# hnl = 0.17
-# hnlname = 'HNL'
-# aud = 1.9622
-# audname = 'AUD'
-# mad = 0.208
-# madname = 'MAD'
-# countries = [rub,ars,hnl,aud,mad]
-# def convertation(a):
-#     if a == rub:
-#         print(f'I will get {a*coins} {rubname} from the sale of {coins} conicoins.')
-#     if a == ars:
-#         print(f'I will get {a*coins} {arsname} from the sale of {coins} conicoins.')
-#     if a == hnl:
-#         print(f'I will get {a*coins} {hnlname} from the sale of {coins} conicoins.')
-#     if a == aud:
-#         print(f'I will get {a*coins} {audname} from the sale of {coins} conicoins.')
-#     if a == mad:
-#         print(f'I will get {a*coins} {madname} from the sale of {coins} conicoins.')
-#
-# for i in countries:
-#     convertation(i)
 
 import requests
 import json
@@ -64,10 +37,6 @@ while True:
         print(f'You received {round(convert,2)} {currency}.')
 
     else:
-        # getall = requests.get(f'http://www.floatrates.com/daily/{basecurrency}.json')
-        # jsonall = json.loads(getall.text)
-        # print(len(jsonall))
-        # cashall = {}
         if len(cashall) > 0:
             for key in cashall:
                 if key == currency:
@@ -85,8 +54,6 @@ while True:
             print('Sorry, but it is not in the cache!')
             print(f'You received {round(convert,2)} {currency}.''')
             cashall[currency] = jsonall[trueform]['rate']
-            # print(cashall)
-            # cashall['lsl'] = 'try'
-            # print(len(cashall))
+
     if len(cashall) == len(jsonall):
         break
